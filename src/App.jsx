8220f06b7cfbc961938d36db293d19ef0fcc8e6f
wandasvt <wandaforone@gmail.com>
2026-05-06 16:59:05 +0700

    app.jsx navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
+import Home from "./pages/Home";
 import Hasil from "./pages/Hasil";
 import History from "./pages/History";
 import Article from "./pages/Article";
@@ -9,6 +10,7 @@ import About from "./pages/About";
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/hasil" element={<Hasil />} />
