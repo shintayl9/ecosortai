060b6c68b271e42fc20a91629b170fe1cde86512
Shinta Yulistiana <shintayulistiana2004@gmail.com>
2026-05-08 10:46:52 +0700

    fix navbar + home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,54 @@
 import "../styles/navbar.css";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 // render tampilan navbar
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [activeLink, setActive] = useState("/");
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const handleLinkClick = (path) => {
+        setActiveLink(path);
+        setIsOpen(false);
+    }
+
+    // lock scroll
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, [isOpen]);
+
     return (
         <nav className="navbar">
-            {/* Logo pojok kiri atas */}
-            <h2>EcoSortAI</h2>
+            {/* Logo pojok kiri atas - bisa di klik ke home*/}
+            <Link to="/" className="logo" onClick={() => setIsOpen(false)}>
+                <h2>EcoSortAI</h2>
+            </Link>
 
             {/* Menu Navigasi */}
-            <div className="nav-links">
-                <a href="#">Home</a>
-                <a href="#">How It Works</a>
-                <a href="#">Upload</a>
-                <a href="#">History</a>
-                <a href="#">About</a>
+            <div className={`nav-links ${isOpen ? "active" : ""}`}>
+                <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
+                <Link to="/how-it-works" onClick={() => setIsOpen(false)}>How It Works</Link>
+                <Link to="/upload" onClick={() => setIsOpen(false)}>Upload</Link>
+                <Link to="/history" onClick={() => setIsOpen(false)}>History</Link>
+                <Link to="/about" onClick={() => setIsOpen(false)}>About</Link>
+            </div>
+
+            {/* Hamburger */}
+            <div className={`hamburger ${isOpen ? "active" : ""}`} onClick={toggleMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
             </div>
         </nav>
     );
